f7a7410feac99c2cd3eb59d30a4a923252aa973d
El_Savoury <ross_morgan@outlook.com>
2026-02-08 23:20:17 +0000

    Added missing files

---
+++ Assets/AssetManager.cs
@@ -0,0 +1,60 @@
+﻿using MonogameLibrary.Graphics;
+using MonogameLibrary.Utilities;
+
+namespace MonogameLibrary.Assets
+{
+    public class AssetManager : Singleton<AssetManager>
+    {
+        ContentManager _content;
+
+        private readonly Dictionary<string, TextureAtlas> _spritesheets = [];
+        private readonly Dictionary<string, SpriteFont> _fonts = [];
+
+
+        public void Init(ContentManager content)
+        {
+            _content = content;
+        }
+
+        public T Load<T>(string filePath)
+        {
+            return _content.Load<T>(filePath);
+        }
+
+
+        public void AddSpritesheet(string name, string filePath)
+        {
+            _spritesheets.Add(name, Load<TextureAtlas>(filePath));
+        }
+
+
+        public TextureAtlas GetSpritesheet(string name)
+        {
+            return _spritesheets[name];
+        }
+
+
+        public void RemoveSpritesheet(string name)
+        {
+            _spritesheets.Remove(name);
+        }
+
+
+        public void AddFont(string name, string filePath)
+        {
+            _fonts.Add(name, Load<SpriteFont>(filePath));
+        }
+
+
+        public SpriteFont GetFont(string name)
+        {
+            return _fonts[name];
+        }
+
+
+        public void RemoveFont(string name)
+        {
+            _fonts.Remove(name);
+        }
+    }
+}
